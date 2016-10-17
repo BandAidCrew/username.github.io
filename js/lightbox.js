74cdd83cb07b8a4f5c4650073e61194d7eda1687
Henry Skorpe Sjøen <henryskorpe@gmail.com>
2016-10-17 19:33:07 +0200

    Changes to be committed: 	new file:   .eslintrc.js 		=> Defining linting rules. We use google's es-linting rules 	modified:   js/lightbox.js 		=> Linting complete! 	new file:   package.json 		-Node package file. => Info about the project + dependencies

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,48 +1,53 @@
-// Wait for the browser to render
-window.onload = function(){
+/* global window */
+/* global document */
 
+// Wait for the browser to render
+window.onload = function() {
 // Add a hidden div over the screen.
 // This will be for the lightbox content at a later stage
-let lightbox = document.createElement("div");
-lightbox.id = "lightbox";
+  let lightbox = document.createElement("div");
+  lightbox.id = "lightbox";
 // lightbox.style.display = "hidden";
-let content = document.createElement("div");
-content.id = "lightboxContent";
-//Add the text node to the newly created div
-lightbox.appendChild(content);
+  let content = document.createElement("div");
+  content.id = "lightboxContent";
+// Add the text node to the newly created div
+  lightbox.appendChild(content);
 
 // add the newly created element and its content into the dom
-document.body.appendChild(lightbox);
-
+  document.body.appendChild(lightbox);
 
 // Get all photos with class lightbox
-let photos = document.getElementsByClassName("lightbox");
+  let photos = document.getElementsByClassName("lightbox");
 
-
-//iterate over the photos and add onclick property to each element
-for (var photo in photos) {
-  console.log("tagged: ",photo);
-  photos[photo].id = "img "+photo;
-  photos[photo].onclick = function(e){
-    showImg(this);
+// iterate over the photos and add onclick property to each element
+  for (let photo in photos) {
+    if (photos.hasOwnProperty(photo)) {
+      console.log("tagged: ", photo);
+      photos[photo].id = "img " + photo;
+      photos[photo].onclick = function(e) {
+        showImg(this);
+      };
+    }
   }
-}
+};
 
-}
-
-// lightbox function
-// When choosing an image or video:
-// 1. it should enlarge and center on the screen,
-// 2. the background should be in grayscale.
+/**
+ * Shows a given img in the lightbox
+ * @param {object} imgObj
+ * lightbox function
+ * When choosing an image or video:
+ * 1. it should enlarge and center on the screen,
+ * 2. the background should be in grayscale.
+ */
 function showImg(imgObj) {
-  //find img src and add img to lightbox
+  // find img src and add img to lightbox
   console.log(imgObj);
   let lightbox = document.getElementById("lightboxContent");
-  lightbox.innerHTML = '<img src="'+imgObj.src+'"/>';
-  documentDiv =  document.getElementById("lightbox")
+  lightbox.innerHTML = '<img src="' + imgObj.src + '"/>';
+  let documentDiv = document.getElementById("lightbox");
   documentDiv.style.visibility = "visible";
-  
-  documentDiv.onclick = function(e){
-  	documentDiv.style.visibility = "hidden";	
-  }
+
+  documentDiv.onclick = function(e) {
+    documentDiv.style.visibility = "hidden";
+  };
 }
