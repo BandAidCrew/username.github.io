59f7cbb66cd97e2edec52b04ff62c36d3883bf5b
Daniel Lundeby <Daniel Lundeby>
2016-11-04 18:24:06 +0100

    Textbox now in principle works.

--- js/member_info.js
+++ js/member_info.js
@@ -1,45 +1,42 @@
 window.addEventListener("load", function() {
-    const INFO1 = "ABC";
-    const INFO2 = "DEF";
-    const INFO3 = "GHI";
-    const INFO4 = "JKL";
 
-    let box1 = document.getElementById("member1");
-    let box2 = document.getElementById("member2");
-    let box3 = document.getElementById("member3");
-    let box4 = document.getElementById("member4");
+    let boxes = [];
+    boxes.push(document.getElementById("member1"));
+    boxes.push(document.getElementById("member2"));
+    boxes.push(document.getElementById("member3"));
+    boxes.push(document.getElementById("member4"));
+
     let members = document.getElementById("members");
 
-    addListener(box1, INFO1);
-    addListener(box2, INFO2);
-    addListener(box3, INFO3);
-    addListener(box4, INFO4);
+    for (let box of boxes) {
+        addListener(box);
+    }
 
-    function addListener(box, text) {
-        box.addEventListener("click", function () {
-            let textbox = document.getElementById("textbox");
-            let oldClass = null;
-            if (textbox != null) {
-                oldClass = textbox.className;
-                // If textbox exists, it should be removed
-                textbox.remove();
+    function addListener(box) {
+        box.addEventListener("click", function (event) {
+
+            let textbox = box.getElementsByClassName("textbox")[0];
+            console.log(textbox);
+
+            let newDisplay;
+            let oldDisplay = window.getComputedStyle(textbox).display;
+            if (oldDisplay == "initial") {
+                newDisplay = "none";
+            }
+            else if (oldDisplay == "none") {
+                newDisplay = "block";
             }
 
-            // Add textbox if a new box is clicked
-            if (oldClass != box.id) {
-                addTextbox(box, text);
+            for (elem of boxes) {
+                let tb = elem.getElementsByClassName("textbox")[0];
+                tb.style.display = "none";
             }
+
+            textbox.style.display = newDisplay;
+
+            console.log();
 
         })
     }
-
-    function addTextbox(box, text) {
-        let textbox = document.createElement("div");
-        textbox.id = "textbox";
-        textbox.classList.add(box.id);
-        textbox.innerHTML = text;
-        members.appendChild(textbox);
-    }
-
 });
 
