6e8c8d3187ac13e94904da65ab90d15e3b9fecd1
Daniel Martin Lundeby <daniel.m.lundeby@gmail.com>
2016-11-11 16:09:38 +0100

    Added sticky header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+// Stick the header to the top of the page when scrolling past the header region
+window.addEventListener('scroll', function() {
+    let menu_bar = document.getElementById('menu-bar');
+    let menu_bar_replacement = document.getElementById('menu-bar-replacement');
+    menu_bar_replacement.style.height = "68px";
+    let y_pos = window.pageYOffset;
+    console.log(y_pos);
+    if (y_pos > 240) {
+        menu_bar.style.position = "fixed";
+        menu_bar_replacement.style.display = "block";
+    }
+    else {
+        menu_bar.style.position = "static";
+        menu_bar_replacement.style.display = "none";
+    }
+});
