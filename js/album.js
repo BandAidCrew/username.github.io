1a62264bf947495db00dbd9260552318086d7705
Magnus Nesje Vatne <magnuvat@stud.ntnu.no>
2016-11-11 16:41:50 +0100

    Added documentation

--- js/album.js
+++ js/album.js
@@ -1,3 +1,12 @@
+/*
+FILE NAME: album.js
+WRITTEN BY:  Magnus Nesje Vatne (with help from w3schools)
+WHEN:  November 2016
+PURPOSE:  JavaScript for viewing a slideshow when a picture is clicked and moving between the pictures in the album
+          This code was inspired by w3schools (http://www.w3schools.com/w3css/w3css_slideshow.asp) and modified for use in this context
+*/
+
+
 function openModal() {
   document.getElementById('myModal').style.display = "block";
 }
